b3a2e8b243ebbf5c2c26da0cb16b6a7bd48e67f4
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-06 18:12:53 +0300

    quotenext

--- components/quotes/QuoteList.js
+++ components/quotes/QuoteList.js
@@ -6,16 +6,18 @@ import classes from "./QuoteList.module.css";
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
     if (ascending) {
-      return quoteA.id > quoteB.id ? 1 : -1;
-    } else {
       return quoteA.id < quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id > quoteB.id ? 1 : -1;
     }
   });
 };
 
 const QuoteList = props => {
   const router = useRouter();
-  const isSortAscending = router.query?.sort === "asc";
+  const isSortAscending = router.query.sort
+    ? router.query.sort === "asc"
+    : true;
   const sortedQuotes = sortQuotes(props.quotes, isSortAscending);
   const changeSortingHandler = () => {
     router.push(`/?sort=${isSortAscending ? "desc" : "asc"}`, undefined, {
